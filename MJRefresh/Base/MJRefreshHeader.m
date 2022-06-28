2068b43c7e55cd0518ec0f3e8309d8ff3b6b53a3
Andrey Karaulov <aka@aiweapps.com>
2022-06-28 17:19:42 +0300

    prevent crash

--- MJRefresh/Base/MJRefreshHeader.m
+++ MJRefresh/Base/MJRefreshHeader.m
@@ -68,7 +68,8 @@ NSString * const MJRefreshHeaderRefreshingBoundsKey = @"MJRefreshHeaderRefreshin
     insetT = insetT > self.mj_h + _scrollViewOriginalInset.top ? self.mj_h + _scrollViewOriginalInset.top : insetT;
     self.insetTDelta = _scrollViewOriginalInset.top - insetT;
     // 避免 CollectionView 在使用根据 Autolayout 和 内容自动伸缩 Cell, 刷新时导致的 Layout 异常渲染问题
-    if (self.scrollView.mj_insetT != insetT) {
+    CGFloat diff = fabs(self.scrollView.mj_insetT - insetT);
+    if (diff > 1) {
         self.scrollView.mj_insetT = insetT;
     }
 }
